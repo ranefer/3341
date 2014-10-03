d1851fba37911d75324bf7b3398ec09246a59fba
Nate <Nate@Sul>
2014-10-03 18:34:02 -0400

    Statement complete

--- Tokenizer/src/Statement.java
+++ Tokenizer/src/Statement.java
@@ -1,13 +1,6 @@
 import java.util.ArrayList;
 
 public class Statement {
-	private static final String[] RESERVED = { "if", "while", "loop", "assign",
-			"read", "write" };
-
-	public static void parse(Token tokens, ArrayList<Integer> t) {
-		assert (isStatement(tokens.current()));
-
-	}
 
 	public static boolean isStatement(String token) {
 		boolean result = false;
@@ -15,5 +8,27 @@ public class Statement {
 			result = result || token.equals(s);
 		}
 		return result;
+	}
+
+	private static final String[] RESERVED = { "if", "while", "loop", "assign",
+			"read", "write" };
+
+	public static void parse(Token tokens, ArrayList<Integer> t) {
+		assert (isStatement(tokens.current()));
+        if(Assign.isAssign(tokens.current()))
+        	Assign.parse(tokens, t);
+		else if(If.isIf(tokens.current())) 
+			If.parse(tokens, t);
+		else if(Loop.isLoop(tokens.current())) 
+			Loop.parse(tokens, t);
+		else if(Input.isInput(tokens.current())) 
+			Input.parse(tokens, t);
+		else if(Output.isOutput(tokens.current())) 
+			Output.parse(tokens, t);
+		else 
+			assert(false);
+
+        if(tokens.hasCurrent() && isStatement(tokens.current()))
+        	Statement.parse(tokens, t);
 	}
 }
