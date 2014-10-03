c66965ccf642a1b7fc61d180df0e6400d97dfcfd
Nate <Nate@Sul>
2014-10-03 19:37:01 -0400

    Expression finished

--- Tokenizer/src/Expression.java
+++ Tokenizer/src/Expression.java
@@ -1,15 +1,25 @@
 import java.util.ArrayList;
 
-
 public class Expression {
 
 	public static boolean isExpression (String token){
-		//TODO
-		return false;
+		return Factor.isFactor(token);
 	}
 
 	public static void parse(Token tokens, ArrayList<Integer> t) {
-
+		assert(isExpression(tokens.current()));
+		
+		Factor.parse(tokens, t);
+		
+		if(tokens.hasCurrent() && tokens.current().equals("+")) {
+			t.add(22);
+        	assert(tokens.hasNext() && Expression.isExpression(tokens.current()));
+        	Expression.parse(tokens, t);
+		} else if(tokens.hasCurrent() && tokens.current().equals("-")) {
+			t.add(23);
+            assert(tokens.hasNext() && Expression.isExpression(tokens.current()));
+            Expression.parse(tokens, t);
+		} else 
+			assert(false) : "Expected +/-";
 	}
-
 }
--- Tokenizer/src/Factor.java
+++ Tokenizer/src/Factor.java
@@ -1,9 +1,14 @@
+import java.util.ArrayList;
+
 public class Factor {
+
+	public static boolean isFactor (String token) {
+		//TODO:
+		return false;
+	}
 
 	public static void parse(Token tokens, ArrayList<Integer> t) {
 
 	}
-	public static boolean isFactor (String token) {
-		
-	}
+
 }
