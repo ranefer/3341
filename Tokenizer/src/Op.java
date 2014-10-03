0d88181a4861773604e45001d756d2c6cbe8b6fa
Nate <Nate@Sul>
2014-10-03 19:47:27 -0400

    Op completed

--- Tokenizer/src/Op.java
+++ Tokenizer/src/Op.java
@@ -13,6 +13,20 @@ public class Op {
 	}
 
 	public static void parse(Token tokens, ArrayList<Integer> t) {
+		assert(isOp(tokens.current()));
 		
+		String alternative = tokens.current();
+	
+		if(alternative.matches("[0-9]+"))  {
+			t.add(31); // integer
+		} else if(Id.isId(alternative)) {
+			Id.parse(tokens, t);
+		} else if(alternative.equals("(")) {
+			t.add(20); // (
+			Expression.parse(tokens, t);
+			assert(tokens.current().equals(")")) : "Expected ')'";
+			t.add(21); // )
+		} else 
+			assert(false) : "Expected Op";
 	}
 }
