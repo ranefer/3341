40fadd9df6dbe079011cdbd1361f89af48390682
Nate <Nate@Sul>
2014-10-03 19:22:19 -0400

    condition complete

--- Tokenizer/src/Condition.java
+++ Tokenizer/src/Condition.java
@@ -1,6 +1,14 @@
 import java.util.ArrayList;
 
 public class Condition {
+	
+	public static boolean isCondition(String token) {
+		boolean result = false;
+		result = result || token.equals("!");
+		result = result || token.equals("[");
+		result = result || Comparison.isComparison(token);
+		return result;
+	}
 
 	public static void parse(Token tokens, ArrayList<Integer> t) {
 		assert (isCondition(tokens.current()));
@@ -12,7 +20,6 @@ public class Condition {
 		} else if (alternative.equals("!")) {
 			parseExclamationPoint(tokens, t);
 			Condition.parse(tokens, t);
-
 		} else if (alternative.equals("[")) {
 			parseOpenBracket(tokens, t);
 			Condition.parse(tokens, t);
@@ -20,46 +27,36 @@ public class Condition {
 			Condition.parse(tokens, t);
 			parseClosedBracket(tokens, t);
 		}
+	}
+
+	public static void parseExclamationPoint(Token tokens, ArrayList<Integer> t) {
+		assert (tokens.current().equals("!")) : "Expected '!'";
+		t.add(15); // !
 
 		tokens.skip();
 	}
 
-	public static boolean isCondition(String token) {
-		boolean result = false;
-		result = result || token.equals("!");
-		result = result || token.equals("[");
-		result = result || Comparison.isComparison(token);
-		return result;
-	}
-
 	public static void parseOpenBracket(Token tokens, ArrayList<Integer> t) {
+		assert(tokens.current().equals("[")) : "Expected '[";
+		t.add(16);
+
 		tokens.skip();
 	}
 
 	public static void parseClosedBracket(Token tokens, ArrayList<Integer> t) {
-		String bracket = "";
-		if (tokens.hasNext())
-			bracket = tokens.current();
-		assert (bracket.equals("[")) : "Expected '[' but was " + bracket;
-
-		tokens.skip();
-	}
-
-	public static void parseExclamationPoint(Token tokens, ArrayList<Integer> t) {
-		String bracket = "";
-		if (tokens.hasNext())
-			bracket = tokens.current();
-		assert (bracket.equals("]")) : "Expected ']' but was " + bracket;
-
+		assert (tokens.current().equals("]")) : "Expected ']'";
+		t.add(17);
+		
 		tokens.skip();
 	}
 
 	public static void parseSymbol(Token tokens, ArrayList<Integer> t) {
-		String symbol = "";
-		if (tokens.hasNext())
-			symbol = tokens.current();
-		assert (symbol.equals("&&") || symbol.equals("||")) : "Expected '&&' or '||' but was "
-				+ symbol;
+		if(tokens.current().equals("&&"))
+			t.add(18); // &&
+		else if(tokens.current().equals("||"))//18
+			t.add(19); // ||
+		else
+            assert (false) : "Expected '&&' or '||'";
 
 		tokens.skip();
 	}
