3036e359a7dd9ec708ab47e7675613d8e00b2aa8
Nate <Nate@Sul>
2014-10-03 18:10:00 -0400

    Program complete

--- Tokenizer/src/Program.java
+++ Tokenizer/src/Program.java
@@ -6,53 +6,39 @@ public class Program {
 
 public final String[] RESERVED = {"program", "begin", "end", "if", "int", "if", "then", "else", "while", "loop", "read", "write"};
 
+	public static boolean isProgram(String token) {
+		return token.equals("program");
+	}
+	
 	public static void parse(Token tokens, ArrayList<Integer> t) {
-		parseProgram(tokens, t);
+		assert(isProgram(tokens.current())): "Expected: program but was '" + tokens.current() + "'";;
+		t.add(1); // program
 		
-		if(Declarations.isDeclaration(tokens.current()))
-            Declarations.parse(tokens, t);
+        assert(tokens.hasNext() && Declarations.isDeclaration(tokens.current())) : "Expected delcaration";
+		Declarations.parse(tokens, t);
 
-		parseBegin(tokens, t);
+		assert(tokens.hasCurrent()) : "Exptectd 'begin'";
+        parseBegin(tokens, t);
 		
-		if(StatementSequence.isStatementSequence(tokens.current()))
-            StatementSequence.parse(tokens, t);
+		assert(tokens.hasCurrent() && StatementSequence.isStatementSequence(tokens.current())) : "Expected Statement Sequence";
+        StatementSequence.parse(tokens, t);
 		
+        assert(tokens.hasCurrent()) : "Exptected 'end'";
 		parseEnd(tokens, t);
 
-    	t.add(33); // EOF
     	assert(!tokens.hasNext()) : "Expected end of file";
+    	t.add(33); // EOF
+	}
 
-	}
-	
-	private static void parseProgram(Token tokens, ArrayList<Integer> t) {
-		String program = "";
-		if(tokens.hasNext()) 
-        	program = tokens.current();
-		assert(program.equals("program")) : "Expected: program but was '" + program + "'";
-		
-		t.add(1); // program
-		tokens.skip();
-	}
-	
 	private static void parseBegin(Token tokens, ArrayList<Integer> t) {
-        String begin = "";
-		if(tokens.hasNext())
-			begin = tokens.current();
-		assert(begin.equals("begin")) : "Expected: begin but was '" + begin + "'";
-		
+		assert(tokens.current().equals("begin")) : "Expected: begin but was '" + tokens.current() + "'";
 		t.add(2); // begin
 		tokens.skip();
 	}
 
 	private static void parseEnd(Token tokens, ArrayList<Integer> t) {
-		String end = "";
-		if(tokens.hasNext())
-			end = tokens.current();
-		assert(end.equals("end")) : "Expected: end but was '" + end + "'";
-		
+		assert(tokens.current().equals("end")) : "Expected: end but was '" + tokens.current() + "'";
 		t.add(3); // end
 		tokens.skip();
 	}
-	
-	
 }
