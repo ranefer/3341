ad1c58f67918ebf734190a617e1edd6b0cd12156
Nate <Nate@Sul>
2014-10-03 19:12:07 -0400

    Output complete

--- Tokenizer/src/Output.java
+++ Tokenizer/src/Output.java
@@ -3,12 +3,17 @@ import java.util.ArrayList;
 public class Output {
 
 	public static boolean isOutput (String token) {
-		//TODO
-		return false;
+		return token.equals("write");
 	}
-	
+
 	public static void parse(Token tokens, ArrayList<Integer> t) {
-
+		assert(isOutput(tokens.current()));
+		tokens.skip();
+		
+		assert(tokens.hasCurrent() && Id.isId(tokens.current())) : "Expected id";
+		Id.parse(tokens, t);
+		
+		assert(tokens.hasCurrent() && Colon.isColon(tokens.current())) : "Expected ';'";
+		Colon.parse(tokens, t);
 	}
-
 }
