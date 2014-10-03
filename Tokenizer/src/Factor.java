6a02a7b1b7df1afeea49d261de5f7217a12a7313
Nate <Nate@Sul>
2014-10-03 19:40:20 -0400

    Factor complete

--- Tokenizer/src/Factor.java
+++ Tokenizer/src/Factor.java
@@ -3,12 +3,17 @@ import java.util.ArrayList;
 public class Factor {
 
 	public static boolean isFactor (String token) {
-		//TODO:
-		return false;
+		return Op.isOp(token); // Factor = { Op | Op * Factor }
 	}
 
 	public static void parse(Token tokens, ArrayList<Integer> t) {
-
+		assert(isFactor(tokens.current()));
+		Op.parse(tokens, t);
+		
+		if(tokens.hasCurrent() && tokens.equals("*")) {
+			t.add(24);
+			assert(Factor.isFactor(tokens.current()));
+			Factor.parse(tokens, t);
+		}
 	}
-
 }
